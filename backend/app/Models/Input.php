6892c2f4455aa66845455687d44a93240a7492fb
TonnyCruise <tonnycruiseomondi@gmail.com>
2026-03-16 17:51:46 +0300

    Added farm inputs table and madel

---
+++ backend/app/Models/Input.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Input extends Model
+{
+    protected $fillable = [
+        'name',
+        'type',
+        'unit',
+        'cost_per_unit'
+    ];
+}
